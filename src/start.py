4c2a34dcb9ee2015d402cd64b64244c1a4bc02b2
st-lnad <46500729+st-lnad@users.noreply.github.com>
2020-12-13 18:36:51 +0300

    Add oracle listener

---
+++ src/start.py
@@ -0,0 +1,21 @@
+from oracle.blockchain_functions.py import parse_site, configure_contract
+my_filter = my_contract.eventFilter('Beacon', {'fromBlock': 0,'toBlock': 'latest'})
+eventlist = my_filter.get_all_entries()
+
+
+def handle_event(contract):
+    parse_answer = parse_site()
+    return_result_to_contract(contract, parse_answer)
+
+
+def listen_contract_event(contract, beacon_filter, interval):
+    while True:
+        for event in beacon_filter.get_new_entries():
+            handle_event(contract)
+            time.sleep(interval)
+
+
+if __name__ == '__main__':
+    contract = configure_contract()
+    beacon_filter = contract.eventFilter('Beacon', {'fromBlock': 0, 'toBlock': 'latest'})
+    listen_contract_event(contract=contract, beacon_filter=beacon_filter, interval=1)
