38a4379b33f64cb20108f7d2cd4607cc73e0656f
ch4t5ky <mr_a953@mail.ru>
2020-12-13 17:39:14 +0300

    Add some files for template oracle

--- src/oracle/blockchain_functions.py
+++ src/oracle/blockchain_functions.py
@@ -1,6 +1,6 @@
 from web3 import Web3
 from web3.middleware import geth_poa_middleware
-from constants import CONTRACT_ADDRESS, HTTP_PROVIDER, ABI_PATH, BIN_PATH, TARGET_URL
+from constants import CONTRACT_ADDRESS, HTTP_PROVIDER, ABI_PATH, BIN_PATH, TARGET_URL, PRIVATE_KEY
 from helper import configure_abi, configure_bytecode
 from parser import cut_cbr_site, get_html_code
 
@@ -19,4 +19,18 @@ def parse_site():
     res = cut_cbr_site(text)
     return res
 
-run_parser()
+def return_result_to_contract(contract, res):
+    tx = contract.functions.set_Silver("56.15").buildTransaction({'gas': 3000000, 'nonce': web3.eth.getTransactionCount("0x607Ec28AB2418F697D4B40E6e1A8Cf7F0fea4596")})
+    signed_tx = web3.eth.account.signTransaction(tx, PRIVATE_KEY)
+    tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+    print('Registration request sent by '+tx_hash.hex())
+
+def changeOwner(contract):
+    tx = contract.functions.changeOwner("0x607Ec28AB2418F697D4B40E6e1A8Cf7F0fea4596").buildTransaction({'gas': 3000000, 'nonce':web3.eth.getTransactionCount("0x607Ec28AB2418F697D4B40E6e1A8Cf7F0fea4596") })
+    signed_tx = web3.eth.account.signTransaction(tx, PRIVATE_KEY)
+    tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+    print('Registration request sent by '+tx_hash.hex())
+
+contract = configure_contract()
+res = parse_site()
+print(res)
--- src/oracle/constants.py
+++ src/oracle/constants.py
@@ -1,7 +1,8 @@
 import os
 
-HTTP_PROVIDER = os.environ.get("HTTP_PROVIDER", "https://rpc.slock.it/goerli")
-CONTRACT_ADDRESS = os.environ.get("CONTRACT_ADDRESS","0x31381BB193fCE17bCDdA9f32bA1E993148eD49Ee")
+HTTP_PROVIDER = os.environ.get("HTTP_PROVIDER", "")
+CONTRACT_ADDRESS = os.environ.get("CONTRACT_ADDRESS","")
 ABI_PATH = os.environ.get("ABI_PATH", "")
 BIN_PATH = os.environ.get("BIN_PATH", "")
-TARGET_URL = os.environ.get("TARGET_URL", "https://cbr.ru/eng/key-indicators/")
+TARGET_URL = os.environ.get("TARGET_URL", "")
+PRIVATE_KEY = os.environ.get("PRIVATE_KEY", "")
